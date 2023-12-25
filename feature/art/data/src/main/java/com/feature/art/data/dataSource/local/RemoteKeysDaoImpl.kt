f3aa742583f3ba045014b306f40bbaeea5971b6c
Farid Benhaimoud <farid.benhaimoud@gmail.com>
2023-12-25 16:53:06 +0100

    Some decoupling optimization

--- feature/art/data/src/main/java/com/feature/art/data/dataSource/local/RemoteKeysDaoImpl.kt
+++ feature/art/data/src/main/java/com/feature/art/data/dataSource/local/RemoteKeysDaoImpl.kt
@@ -5,20 +5,20 @@ import com.core.cache.dataSource.database.ArtDatabase
 import com.feature.common.domain.entity.art.RemoteKeysEntity
 import javax.inject.Inject
 
-class RemoteKeysDaoImpl @Inject constructor(private val artDatabase: ArtDatabase): RemoteKeysDao {
+class RemoteKeysDaoImpl @Inject constructor(private val database: ArtDatabase): RemoteKeysDao {
     override suspend fun insertAll(remoteKey: List<RemoteKeysEntity>) {
-        TODO("Not yet implemented")
+        database.getRemoteKeysDao().insertAll(remoteKey)
     }
 
     override suspend fun getRemoteKeyByArtObjectID(id: String): RemoteKeysEntity? {
-        TODO("Not yet implemented")
+        return database.getRemoteKeysDao().getRemoteKeyByArtObjectID(id)
     }
 
     override suspend fun clearRemoteKeys() {
-        TODO("Not yet implemented")
+        database.getRemoteKeysDao().clearRemoteKeys()
     }
 
     override suspend fun getCreationTime(): Long? {
-        TODO("Not yet implemented")
+        return database.getRemoteKeysDao().getCreationTime()
     }
 }
